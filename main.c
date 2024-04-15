0ad09b55696913c23945775251913a33a9ce7c69
GlipAmongus <108809268+GlipAmongus@users.noreply.github.com>
2024-04-15 21:12:23 +0200

    Questions 2 and 3 finished

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 #include <stdbool.h>
 
 #define SIZE 256
-#define MAX 1025
+#define N 16
 
-void ShellSort(int A[]);
-void QuickSort(int B[], int first, int last);
-int Partition(int B[], int first, int last);
+void ShellSort(int a[]);
+void QuickSort(int b[], int first, int last);
+int Partition(int b[], int first, int last);
+int extreme_points(int const ARRAY[]);
 
-void Question1(int A[], int B[]);
+void Question1(int a[], int b[]);
+void Question2(int const A[], int const B[], int c[]);
+void Question3();
 
 int main()
 {
@@ -18,41 +21,123 @@ int main()
         srand(time(NULL));
 
         //arrays used in question 1
-        int A[SIZE];
-        int B[SIZE*2];
+        int a[SIZE];
+        int b[SIZE*2];
 
         //array used in question 2
-        int C[SIZE*3];
+        int c[SIZE*3];
 
-        Question1(A, B);
-        printf("[");
-        for(int i = 0; i < SIZE; i++)
-        {
-            printf("%i , ", A[i]);
-        }
-        printf("]");
-        printf("[");
-        for(int i = 0; i < SIZE*2; i++)
-        {
-            printf("%i , ", B[i]);
-        }
-        printf("]");
-
-
+        Question1(a, b);
+        Question2(a, b, c);
+        Question3();
 }
 
-void Question1(int A[], int B[])
+
+void Question1(int a[], int b[])
 {
+    int const MAX = 1025;
+
     //Populate arrays with random numbers (0 + 1024)
     for (int i = 0; i < SIZE; i++)
-        A[i] = rand() % MAX;
+        a[i] = rand() % MAX;
 
     for (int i = 0; i < SIZE*2; i++)
-        B[i] = rand() % MAX;
+        b[i] = rand() % MAX;
 
-    ShellSort(A);
+    //Sorting functions
+    ShellSort(a);
+    QuickSort(b, 0, SIZE*2 - 1);
 
-    QuickSort(B, 0, SIZE * 2 - 1);
+    //Output arrays A, B, in that order
+    printf("[");
+    for(int i = 0; i < SIZE - 1; i++)
+    {
+        printf("%i, ", a[i]);
+    }
+    printf("%i]\n", a[SIZE-1]);
+    printf("[");
+    for(int i = 0; i < SIZE*2 - 1; i++)
+    {
+        printf("%i, ", b[i]);
+    }
+    printf("%i]\n", b[SIZE*2-1]);
+}
+
+void Question2(int const A[], int const B[], int c[])
+{
+    //Populate array C with arrays A and B
+    for(int i = 0; i < SIZE; i++)
+        c[i] = A[i];
+    for(int i = 0; i < SIZE*2; i++)
+        c[i+SIZE] = B[i];
+
+    //Output Array C
+    printf("[");
+    for(int i = 0; i < SIZE*3 - 1; i++)
+    {
+        printf("%i, ", c[i]);
+    }
+    printf("%i]\n", c[SIZE*3-1]);
+}
+
+void Question3()
+{
+    int arr[N];
+    int ext_cnt;
+
+    //Populate array, Duplicates allowed since not enforced otherwise
+    for(int i = 0; i < N; i++)
+        arr[i] = rand() % N;
+
+    //Print array
+    printf("[");
+    for(int i = 0; i < N; i++)
+    {
+        printf("%i, ", arr[i]);
+    }
+    printf("%i]\n", arr[N-1]);
+
+    //Extreme points
+    ext_cnt = extreme_points(arr);
+
+    //Print SORTED when no extreme points are found
+    if(ext_cnt == 0)
+        printf("SORTED\n");
+    /*
+     * An array has no extreme points if and only if it is sorted is false
+     * since there exists a case where the array has no extreme points, and is not sorted:
+     * [1, 5, 5, 1].
+     * */
+
+    /*
+     * Although in the case where and an array is sorted [1,2,3,4,5] or inversely [5,4,3,2,1]
+     * it does not contain any extreme points.
+     * Thus, in the absence of extreme points and duplicates the array is sorted.
+     * */
+}
+
+int extreme_points(int const ARRAY[])
+{
+    int count = 0;
+    int ext_pts[N - 2];
+    for(int i = 1; i < N-1; i++)
+    {
+        if(((ARRAY[i-1] < ARRAY[i]) && (ARRAY[i] > ARRAY[i+1])) || ((ARRAY[i - 1] > ARRAY[i]) && (ARRAY[i] < ARRAY[i + 1])))
+        {
+            ext_pts[count] = ARRAY[i];
+            count++;
+        }
+    }
+
+    //Print extreme points
+    printf("[");
+    for(int i = 0; i < count-2; i++)
+    {
+        printf("%i, ", ext_pts[i]);
+    }
+    printf("%i]\n", ext_pts[count-1]);
+
+    return count;
 }
 
 void ShellSort(int A[])
