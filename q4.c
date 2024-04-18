7a351353e99c0fc1b689dbddc06d9b761ddaf8ec
GlipAmongus <108809268+GlipAmongus@users.noreply.github.com>
2024-04-18 05:10:48 +0200

    q4.c done

--- q4.c
+++ q4.c
@@ -1,3 +1,48 @@
-//
-// Created by User on 18/04/2024.
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    const int N = 256;
+    const int UPPER = 1024;
+
+    int array_n[N];
+    int number;     //temp. random number generated
+    bool match;     //unique constraint
+
+    //Populate arrays with random unique numbers (1 - 1024)
+    for (int i = 0; i < N; i++)
+    {
+        do
+        {
+            match = false;
+            number = (rand() % UPPER) + 1;
+            for(int j = 0; j < i; j++)
+            {
+                if(number == array_n[j])
+                    match = true;
+            }
+        }while(match);
+
+        array_n[i] = number;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            for (int k = j + 1; k < N; k++)
+            {
+                for (int l = k + 1; l < N; l++)
+                {
+                    // Check if product of (number[i], number[j]) is equal to product of (number[k], number[l])
+                    if (array_n[i] * array_n[j] == array_n[k] * array_n[l])
+                        printf("((%d, %d), (%d, %d))\n", array_n[i], array_n[j], array_n[k], array_n[l]);
+                }
+            }
+        }
+    }
+
+    exit(0);
+}
