9e5bfca5053f465252bdfacc8d14febe5137aa1e
GlipAmongus <108809268+GlipAmongus@users.noreply.github.com>
2024-04-18 12:57:51 +0200

    q12.c done

--- q12.c
+++ q12.c
@@ -1,3 +1,37 @@
-//
-// Created by User on 18/04/2024.
-//
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+unsigned long long sumFibonacci(int i);
+
+int main(void)
+{
+    //unique seed value
+    srand(time(NULL));
+
+    int n = (rand() % 127) + 2;
+
+    unsigned long long sum = sumFibonacci(n);
+
+    printf("Sum of the first %d numbers: %llu", n, sum);
+}
+
+unsigned long long sumFibonacci(int i)
+{
+    int sum = 2;
+    int x = 1;
+    int y = 1;
+    int z;
+
+    if(i >= 2) {
+        for (int j = 2; j < i; j++) {
+            z = x + y;
+            x = y;
+            y = z;
+            sum += z;
+        }
+        return sum;
+    }
+
+    return 0;
+}
