44b0456a719052193fb3531338bb4df031ded52b
GlipAmongus <108809268+GlipAmongus@users.noreply.github.com>
2024-05-25 22:57:49 +0200

    includes 'N' in list of primes

--- q6.c
+++ q6.c
@@ -8,10 +8,11 @@ void seiveEratosthene(int capacity);
 
 int main(void)
 {
-    int const N = 256;
+    int const N = 251;
+    printf("Check for primes up to: %d\n", N);
 
     printf("Brute Force: \n");
-    for(int i = 1; i < N; i++)
+    for(int i = 1; i <= N; i++)
     {
         if(prime_boolean(i))
             printf("%d, ", i);
@@ -27,9 +28,9 @@ bool prime_boolean(int n)
 {
     if(n == 1)
         return false;
-    for(int i = 2; i < n; i++)
+    for(int i = 2; i <= n; i++)
     {
-        if(n % i == 0)
+        if((n % i == 0) && (n != i))
             return false;
     }
     return true;
@@ -39,25 +40,23 @@ void seiveEratosthene(int capacity)
 {
     bool prime[capacity];
     memset(prime, true, capacity);
-
-    for(int p = 2; p*p < capacity; p++)
+    for(int p = 2; p*p <= capacity; p++)
     {
-        if(prime[p]){
+        if(prime[p-1]){
             //marking multiples of prime numbers
-            for(int i = p*p; i < capacity; i += p)
+            for(int i = p*p; i <= capacity; i += p)
             {
                 /*
                  * check through multiples of p at p^2 or greater
                  * since multiples of p smaller than p^2 have already been checked
                  * */
-                prime[i] = false;
+                prime[i-1] = false;
             }
         }
     }
-
-    for(int p = 2; p < capacity; p++)
+    for(int p = 2; p <= capacity; p++)
     {
-        if(prime[p])
+        if(prime[p-1])
             printf("%d, ", p);
     }
     printf("\n");
